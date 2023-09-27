bbf1b04c79a9addbe903377766f943baf268b947
Jim MacArthur <jim@mode7.co.uk>
2023-09-27 22:03:34 +0100

    Make a slightly cleaner way of making objects

--- ssem.js
+++ ssem.js
@@ -1,48 +1,68 @@
 'use strict';
 
+function merge(original_hash, added_hash) {
+    Object.keys(added_hash).forEach(function(key) {
+	original_hash[key] = added_hash[key];
+    });
+    return original_hash;
+}
+
+function addFixture(body, shape, physics, collisions) {
+    var properties = {shape: shape};
+    merge(properties, physics);
+    merge(properties, collisions);
+    return body.createFixture(properties);
+}
+
 function createWorld(world) {
+
+    var mass_none = {
+	density: 0.0,
+	friction: 0.3
+    }
+
+    var mass_normal =  {
+	density: 1.0,
+	friction: 0.3
+    }
+
+    var collisions_none = {
+	filterCategoryBits: 0x01,
+	filterMaskBits: 0,
+	filterGroupIndex: -1
+    }
+
+    var collisions_toplayer = {
+	filterCategoryBits: 0x01,
+	filterMaskBits: 0x01,
+	filterGroupIndex: 1
+    }
+
     // Create the ground object
     var ground = world.createBody();
-
-    var groundFD = {
-	filterCategoryBits: 2,
-	filterMaskBits: 0xFFFF,
-	filterGroupIndex: 0,
-    };
     ground.createFixture({
 	shape: new pl.Edge(new Vec2(-50, 0), new Vec2(+50, 0)),
     });
 
     // Create the hopper and injector
     // Injector consists of an active rectangle and an intangible rectangle
-    let body = world.createBody({
+    let injector_lever = world.createBody({
 	type: "dynamic",
 	position: new Vec2(0.0, 4.0)
     });
 
     var fix1 = new Box(2.0, 0.5, new Vec2(1.5,0));
     var fix2 = new Box(0.5, 2.0, new Vec2(0,-1.5));
-    body.createFixture({
-	shape: fix1,
-	density: 0.0,
-	friction: 0.3,
-	filterCategoryBits: 0x01,
-	filterMaskBits: 0x0,
-    });
+    addFixture(injector_lever, fix1, mass_none, collisions_none);
+    addFixture(injector_lever, fix2, mass_normal, collisions_toplayer);
 
-    body.createFixture({
-	shape: fix2,
-	density: 1.0,
-	friction: 0.3,
-    });
-
-    body.shapeOverride = union(fix1, fix2);
+    //injector_lever.shapeOverride = union(fix1, fix2);
 
     var revoluteJoint = world.createJoint(pl.RevoluteJoint({
 	lowerAngle: -0.25 * Math.PI,
 	upperAngle: 0.25 * Math.PI,
 	enableLimit: false,
-    }, ground, body, Vec2(0.0, 4.0)));
+    }, ground, injector_lever, Vec2(0.0, 4.0)));
 
     let ball1 = world.createBody({
 	type: "dynamic",
