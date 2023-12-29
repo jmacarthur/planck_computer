61f8603d9f15a8e220e432a9adaada422e05a0c7
Jim MacArthur <jim+git@mode7.co.uk>
2023-12-29 10:39:45 +0000

    Start to rationalise cam layout

--- ssem.js
+++ ssem.js
@@ -291,6 +291,12 @@ function create_cam(world, ground, xoffset, yoffset) {
     var tab_height = 1;
     var cam = world.createBody({type: "dynamic", position: new Vec2(xoffset, yoffset)});
 
+    // Timing is an array of [ start angle, rise angle, length, fall angle ].
+    // Profile starts at start angle and is at full height by start angle + rise angle.
+    // Profile starts to drop at start angle + rise angle + length
+    // Profile fully low at start angle + rise angle + length + fall angle.
+    var timing = [ [ 0, 0.1, 1.3, 0 ] ];
+
     // Cam base circle
     var fake_circle_shape_points = [];
     var circle_interpolation = 32;
@@ -303,24 +309,25 @@ function create_cam(world, ground, xoffset, yoffset) {
     addFixture(cam, new Circle(base_radius), mass_normal, collisions_toplayer);
 
     // Cam profile
+    var t = 0; // In future, used to iterate over timing array
     var max_segments = 13;
-    var profile_length = 1.3; // Radians!
-    var start_angle = 0;
+    var profile_length = timing[t][2]; // Radians!
+    var start_angle = timing[t][0];
     var low_height = base_radius;
     var high_height = base_radius+tab_height;
-    var rise_angle = 0.1;
-    var fall_angle = 0;
-    var point_array = [new Vec2(Math.cos(start_angle-fall_angle)*low_height, Math.sin(start_angle-fall_angle)*low_height)];
+    var rise_angle = timing[t][1];
+    var fall_angle = timing[t][3];
+    var point_array = [new Vec2(Math.cos(start_angle)*low_height, Math.sin(start_angle)*low_height)];
     for(var i=0;i<(max_segments-3);i++) {
-	point_array.push(new Vec2(Math.cos(start_angle+i*profile_length/(max_segments-4)) * high_height,
-				  Math.sin(start_angle+i*profile_length/(max_segments-4)) * high_height));
+	point_array.push(new Vec2(Math.cos(start_angle+rise_angle+i*profile_length/(max_segments-4)) * high_height,
+				  Math.sin(start_angle+rise_angle+i*profile_length/(max_segments-4)) * high_height));
     }
-    point_array.push(new Vec2(Math.cos(start_angle+profile_length+rise_angle)*low_height, Math.sin(start_angle+profile_length+rise_angle)*low_height));
+    point_array.push(new Vec2(Math.cos(start_angle+profile_length+rise_angle+fall_angle)*low_height, Math.sin(start_angle+profile_length+rise_angle+fall_angle)*low_height));
     var profile_polygon = new Polygon(point_array);
     addFixture(cam, profile_polygon, mass_normal, collisions_toplayer);
     var revoluteJoint = world.createJoint(pl.RevoluteJoint({
 	maxMotorTorque: 10000000,
-	motorSpeed: 0.1,
+	motorSpeed: -0.1,
 	enableMotor: true,
     }, ground, cam, Vec2(xoffset,yoffset)));
 
