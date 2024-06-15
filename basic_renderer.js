b1c0b35657460693982bf214b22d4180c74b03e7
Jim MacArthur <jim+git@mode7.co.uk>
2024-06-15 15:35:57 +0100

    Read out target angle in degrees

--- basic_renderer.js
+++ basic_renderer.js
@@ -184,7 +184,7 @@ class Renderer {
 
 	var accumulator_string = this.getAccumulatorString()
 
-	this.ctx.fillText((this.cam_position/(Math.PI*2)*100).toFixed(1)+"% " + accumulator_string+ " target angle: "+ this.angleTarget, 40, 20);
+	this.ctx.fillText((this.cam_position/(Math.PI*2)*100).toFixed(1)+"% " + accumulator_string+ " target angle: "+ (this.angleTarget*360/Math.PI/2).toFixed(3)+ "°", 40, 20);
 	this.ctx.restore();
 	this.world.active_ball_list = new_balls;
 
